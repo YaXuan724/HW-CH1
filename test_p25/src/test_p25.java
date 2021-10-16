664a9488d1812bd37a34b4bacaf8e966c912eae6
YaXuan724 <t108360120@ntut.org.tw>
2021-10-16 20:25:02 +0800

    HW-CH1

---
+++ test_p25/src/test_p25.java
@@ -0,0 +1,11 @@
+
+public class test_p25 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+		num = 3;
+		System.out.println("numªº­È¬O:"+num);
+	}
+
+}
